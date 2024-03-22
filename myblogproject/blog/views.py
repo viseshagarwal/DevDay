10c5ba5f23f9ff0ab31a27adb39e70cd53874b7b
KD30062000 <159777650+KD30062000@users.noreply.github.com>
2024-03-22 16:16:38 +0530

    1st

---
+++ myblogproject/blog/views.py
@@ -0,0 +1,11 @@
+from rest_framework import generics
+from .models import BlogPost
+from .serializers import BlogPostSerializer
+
+class BlogPostListCreate(generics.ListCreateAPIView):
+    queryset = BlogPost.objects.all()
+    serializer_class = BlogPostSerializer
+
+class BlogPostRetrieveUpdateDestroy(generics.RetrieveUpdateDestroyAPIView):
+    queryset = BlogPost.objects.all()
+    serializer_class = BlogPostSerializer
